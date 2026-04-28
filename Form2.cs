3d728ec0b66920b610d37351848ca8bd50f73ca2
Gabriel <hdzghdz100@gmail.com>
2026-04-28 00:32:34 -0600

    Agregue unas vistas

---
+++ Form2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimulacionTiendaElProfe
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+            cargar();
+        }
+        public void cargar()
+        {
+            panel.Controls.Clear();
+            Vistas.Catalogos catalogos = new Vistas.Catalogos();
+            catalogos.Dock = DockStyle.Fill;
+            panel.Controls.Add(catalogos);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace SimulacionTiendaElProfe
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form2());
         }
     }
 }
